04e636de8a0c8199ea4e0fc80c80c220c3e416f6
shandiru <msddiru@gmail.com>
2025-10-30 20:34:58 +0530

    added

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,31 +1,30 @@
-
-
-
 import React from 'react'
-import Navbar from '../components/Navbar'
-import ServiceSection from '../components/Service'
+
 import HeroSection from '../components/HeroSection'
 import LogoShowcase from '../components/LogoShowcase'
 import About from '../components/Whyus'
-import TestimonialsCarousel from '../components/Testimonials'
+import ServiceSection from '../components/Service'
 import GallerySection from "../components/GallerySection"
+import TestimonialsCarousel from '../components/Testimonials'
+import Faq from "../components/faq"
 import ContactSection from '../components/Contact'
 import FindUs from '../components/Findus'
-import Faq from "../components/faq"
-import Footer from '../components/Footer'
+
 
 const Home = () => {
     return (
         <div>
+           
             <HeroSection />
             <LogoShowcase />
             <About />
             <ServiceSection />
-            <TestimonialsCarousel />
             <GallerySection />
+            <TestimonialsCarousel />
+            <Faq />
             <ContactSection />
             <FindUs />
-            <Faq />
+           
         </div>
     )
 }
